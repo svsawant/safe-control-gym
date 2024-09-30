81f754239ebe120221ece2dd5bd19d82ffb58327
Shambhuraj Sawant <shambhurajsawant10@gmail.com>
2024-09-30 11:42:40 +0200

    WIP on developmental

--- safe_control_gym/math_and_models/transformations.py
+++ safe_control_gym/math_and_models/transformations.py
@@ -243,73 +243,26 @@ def npRotXYZ(phi, theta, psi):
     return R
 
 
-def quaternion_rotation_matrix(q):
-    """
-    Covert a quaternion into a full three-dimensional rotation matrix.
+def get_angular_velocity_rpy(rpy, rpy_rates):
+    '''
+    Convert rpy and rpy_rates to body-frame angular velocity
 
     Input
-    :param q: A 4 element array representing the quaternion (q0,q1,q2,q3)
+      :param rpy: roll, pitch, yaw
+      :param rpy_rates: rates for roll, pitch, yaw
 
     Output
-    :return: A 3x3 element matrix representing the full 3D rotation matrix.
-             This rotation matrix converts a point in the local reference
-             frame to a point in the global reference frame.
-    """
-    # Extract the values from Q
-    q0 = q[0]
-    q1 = q[1]
-    q2 = q[2]
-    q3 = q[3]
-
-    # First row of the rotation matrix
-    r00 = 2 * (q0 * q0 + q1 * q1) - 1
-    r01 = 2 * (q1 * q2 - q0 * q3)
-    r02 = 2 * (q1 * q3 + q0 * q2)
-
-    # Second row of the rotation matrix
-    r10 = 2 * (q1 * q2 + q0 * q3)
-    r11 = 2 * (q0 * q0 + q2 * q2) - 1
-    r12 = 2 * (q2 * q3 - q0 * q1)
-
-    # Third row of the rotation matrix
-    r20 = 2 * (q1 * q3 - q0 * q2)
-    r21 = 2 * (q2 * q3 + q0 * q1)
-    r22 = 2 * (q0 * q0 + q3 * q3) - 1
-
-    # 3x3 rotation matrix
-    rot_matrix = np.array([[r00, r01, r02],
-                           [r10, r11, r12],
-                           [r20, r21, r22]])
-
-    return rot_matrix
-
-
-def euler_from_quaternion(q):
-    """
-    Convert a quaternion into euler angles (roll, pitch, yaw)
-    roll is rotation around x in radians (counterclockwise)
-    pitch is rotation around y in radians (counterclockwise)
-    yaw is rotation around z in radians (counterclockwise)
-    """
-    x, y, z, w = q[0], q[1], q[2], q[3]
-    t0 = +2.0 * (w * x + y * z)
-    t1 = +1.0 - 2.0 * (x * x + y * y)
-    roll_x = math.atan2(t0, t1)
-
-    t2 = +2.0 * (w * y - z * x)
-    t2 = +1.0 if t2 > +1.0 else t2
-    t2 = -1.0 if t2 < -1.0 else t2
-    pitch_y = math.asin(t2)
-
-    t3 = +2.0 * (w * z + x * y)
-    t4 = +1.0 - 2.0 * (y * y + z * z)
-    yaw_z = math.atan2(t3, t4)
-
-    return np.array([roll_x, pitch_y, yaw_z])  # in radians
+      :param ang_v: angular velocity
+    '''
+    phi, theta, psi = rpy[0], rpy[1], rpy[2]
+    ang_v = np.array([[1, 0, -np.sin(theta)],
+                      [0, np.cos(phi), np.sin(phi) * np.cos(theta)],
+                      [0, -np.sin(phi), np.cos(phi) * np.cos(theta)]]) @ rpy_rates
+    return ang_v
 
 
 def get_quaternion_from_euler(rpy):
-    """
+    '''
     Convert an Euler angle to a quaternion.
     roll: The roll (rotation around x-axis) angle in radians.
     pitch: The pitch (rotation around y-axis) angle in radians.
@@ -320,7 +273,7 @@ def get_quaternion_from_euler(rpy):
 
     Output
       :return qx, qy, qz, qw: The orientation in quaternion [x,y,z,w] format
-    """
+    '''
     roll, pitch, yaw = rpy[0], rpy[1], rpy[2]
     qx = np.sin(roll / 2) * np.cos(pitch / 2) * np.cos(yaw / 2) - np.cos(roll / 2) * np.sin(pitch / 2) * np.sin(yaw / 2)
     qy = np.cos(roll / 2) * np.sin(pitch / 2) * np.cos(yaw / 2) + np.sin(roll / 2) * np.cos(pitch / 2) * np.sin(yaw / 2)
@@ -328,14 +281,3 @@ def get_quaternion_from_euler(rpy):
     qw = np.cos(roll / 2) * np.cos(pitch / 2) * np.cos(yaw / 2) + np.sin(roll / 2) * np.sin(pitch / 2) * np.sin(yaw / 2)
 
     return qx, qy, qz, qw
-
-
-def get_angularvelocity_rpy(rpy, rpy_rates):
-    """
-    Convert rpy and rpy_rates to body-frame angular velocity
-    """
-    phi, theta, psi = rpy[0], rpy[1], rpy[2]
-    ang_v = np.array([[1, 0, -np.sin(theta)],
-                      [0, np.cos(phi), np.sin(phi) * np.cos(theta)],
-                      [0, -np.sin(phi), np.cos(phi) * np.cos(theta)]]) @ rpy_rates
-    return ang_v
